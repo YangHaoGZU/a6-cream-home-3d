11ebebb3aacd230152ca914f2aded62fddec1bec
YangHaoGZU <leaayang@hotmail.com>
2026-09-24 16:48:49 +0800

    Update panorama preparation script for Nordic asset revision

--- scripts/prepare-panoramas.mjs
+++ scripts/prepare-panoramas.mjs
@@ -39,7 +39,7 @@ for (const point of panoramaPoints) {
     throw new Error("Incorrect preview size: " + point.id);
   manifest.push({
     id: point.id,
-    revision: "french-local-v3-tinypng",
+    revision: "nordic-r03-tinypng",
     width: meta.width,
     height: meta.height,
     bytes: (await fs.stat(filename)).size,
